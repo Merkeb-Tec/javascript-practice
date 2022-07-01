ad315c9137c6dd175b19afc598ff13fa139a50bc
Merkeb Tecle <merkeb17@gmail.com>
2022-06-30 19:56:49 -0700

    console logged the lenght of name

--- js/script.js
+++ js/script.js
@@ -4,4 +4,9 @@ practise*/
 console.log('Merkeb');
 console.log('sushi');
 
+console.log('Supercalifragilisticexpialidocious'.length);
+console.log(500);//choose any large number
+console.log(500n) //use any number with n integer at end
+console.log(typeof 500);
+console.log(typeof 500n);
 
